b1f00513d10caaeb0ff8914f4207dff90e4a9d99
Kaleb Davis <kalebdavisgithub@gmail.com>
2017-10-29 16:22:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Playlist Scraper
+This is an application to help me manage my music libraries between different
+streaming services, such as Spotify and Apple Music.
---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log('starting server');
+  console.log('Hello world');
+});
