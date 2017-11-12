83752d5cd4bab99e217a529e38a3b0a962bdb784
Kaleb Davis <kalebdavisgithub@gmail.com>
2017-11-11 20:34:36 -0500

    Add basic health check endpoint to test

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log('starting server');
-  console.log('Hello world');
+    console.log('starting server');
 });
+
+app.get('/api/health-check', (req, res) => {
+    res.sendStatus(200);
+});
+
+export default app;
